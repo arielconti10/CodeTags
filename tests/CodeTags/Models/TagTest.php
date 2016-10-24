229aeb5c444112ffe9c8ed2e4f9d24f6b1927b09
vagrant <vagrant@homestead>
2016-10-24 17:12:02 +0000

    first commit

---
+++ tests/CodeTags/Models/TagTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace CodePress\CodeTags\Tests\Models;
+
+
+use CodePress\CodeTags\Models\Tag;
+use CodePress\CodeTags\Tests\AbstractTestCase;
+
+class TagTest extends AbstractTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        $this->migrate();
+    }
+
+    public function test_check_if_a_tag_can_be_persisted()
+    {
+        $tag = Tag::create(['name'=>'Tag Test']);
+        $this->assertEquals('Tag Test', $tag->name);
+
+        $tag = Tag::all()->first();
+        $this->assertEquals('Tag Test', $tag->name);
+    }
+}
